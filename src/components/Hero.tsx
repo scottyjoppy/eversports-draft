9c603c77f68a59ed141e8fcb03001f9ec8c70070
scottyjoppy <alexanderdelisle@yahoo.ca>
2025-04-17 22:00:56 +0200

    Testing a new layout with just subtle inspirations from momence

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ const textContainerVariants = {
     opacity: 1,
     transition: {
       delayChildren: 0.3,
-      staggerChildren: 0.2,
+      staggerChildren: 0.5,
     },
   },
 };
@@ -23,108 +23,116 @@ const artContainerVariants = {
   visible: {
     opacity: 1,
     transition: {
-      delayChildren: 1,
-      staggerChildren: 0.4,
+      delayChildren: 0.8,
+      staggerChildren: 0.2,
     },
   },
 };
 
 const itemVariants = {
-  hidden: { y: 20, opacity: 0 },
+  hidden: { x: -20, opacity: 0 },
   visible: {
-    y: 0,
+    x: 0,
     opacity: 1,
-    transition: { duration: 1 },
+    transition: {
+      type: "spring",
+      bounce: 0.5,
+      duration: 1,
+    },
   },
 };
 
 const Hero = () => {
   return (
-    <section className="overflow-x-hidden justify-center flex">
-      <div className="w-full flex flex-col items-center gap-10">
+    <>
+      <section className="h-screen z-0 relative max-w-7xl">
+        <video
+          src="https://www.eversportsmanager.com/wp-content/uploads/2024/10/ennlfrvideo.mp4"
+          className="fixed left-1/2 -translate-x-1/2 top-0 max-w-[1400px] h-full object-cover"
+          style={{
+            clipPath: "inset(0px 0px 5px 5px)",
+            minWidth: "1400px",
+          }}
+          loop
+          muted
+          autoPlay={false}
+          playsInline
+        />
         <motion.div
           variants={textContainerVariants}
           initial="hidden"
           animate="visible"
-          className="relative w-full h-screen max-h-[700px] flex justify-center items-center"
         >
-          <video
-            src="https://www.eversportsmanager.com/wp-content/uploads/2024/10/ennlfrvideo.mp4"
-            className="absolute left-1/2 transform -translate-x-1/2 top-0 max-w-[1400px] h-full object-cover"
-            style={{
-              clipPath: "inset(0px 0px 5px 5px)",
-              minWidth: "1400px",
-            }}
-            loop
-            muted
-            autoPlay
-            playsInline
-          />
+          <motion.h1
+            variants={itemVariants}
+            className="fixed top-[25%] left-0 sm:left-[5%] md:left-[10%] lg:left-[20%] text-5xl text-white"
+          >
+            Join the movement network
+          </motion.h1>
 
-          <div className="absolute flex flex-col top-1/3 left-1/6 gap-6">
-            <motion.h1 variants={itemVariants} className="text-5xl text-white">
-              Join the movement network
-            </motion.h1>
-            <motion.p variants={itemVariants} className="max-w-2xl text-white">
-              More than a management software. A network of more than 4.800
-              partner studios that share the passion for movement.
-            </motion.p>
-            <motion.div variants={itemVariants}>
-              <Link href="https://www.eversportsmanager.com/demo/">
-                <Button className="w-fit bg-secondary font-bold uppercase text-black rounded-full px-5 py-3">
-                  Book Demo
-                </Button>
-              </Link>
-            </motion.div>
-          </div>
+          <motion.p
+            variants={itemVariants}
+            className="fixed top-[35%] left-0 sm:left-[5%] md:left-[10%] lg:left-[20%] max-w-2xl text-white"
+          >
+            More than a management software...
+          </motion.p>
+
+          <motion.div
+            variants={itemVariants}
+            className="fixed top-[45%] left-0 sm:left-[5%] md:left-[10%] lg:left-[20%]"
+          >
+            <Link href="https://www.eversportsmanager.com/demo/">
+              <Button className="bg-secondary font-bold uppercase text-black rounded-full px-5 py-3">
+                Book Demo
+              </Button>
+            </Link>
+          </motion.div>
         </motion.div>
+      </section>
+      </>
+      )};
 
-        {/* Decorative Art Section */}
+      <section className="bg-white/10 border-y-10 border-white h-screen z-10 relative backdrop-blur-2xl"></section>
+      {/* Decorative Art Section */}
+      <motion.div
+        variants={artContainerVariants}
+        initial="hidden"
+        animate="visible"
+        className="relative w-full max-w-5xl my-10 grid overflow-visible"
+        style={{
+          gridTemplateColumns: "1fr",
+          height: "clamp(400px, 50vw, 600px)",
+        }}
+      >
+        <motion.div variants={itemVariants} className="absolute inset-0 z-0">
+          <DesktopCssArt />
+        </motion.div>
         <motion.div
-          variants={artContainerVariants}
-          initial="hidden"
-          animate="visible"
-          className="relative w-full max-w-5xl my-10 grid overflow-visible"
-          style={{
-            gridTemplateColumns: "1fr",
-            height: "clamp(400px, 50vw, 600px)",
-          }}
+          variants={itemVariants}
+          className="absolute z-10 right-[-25vw] sm:right-[-20vw] md:right-[-15vw] lg:right-[-10vw] top-[200px] sm:top-[180px] md:top-[300px]"
         >
-          <motion.div variants={itemVariants} className="absolute inset-0 z-0">
-            <DesktopCssArt />
-          </motion.div>
-
-          <motion.div
-            variants={itemVariants}
-            className="absolute z-10 right-[-25vw] sm:right-[-20vw] md:right-[-15vw] lg:right-[-10vw] top-[200px] sm:top-[180px] md:top-[300px]"
-          >
-            <CalendarCssArt>
-              <Image
-                src="/images/heroMobile.png"
-                alt="eversports logo"
-                fill
-                className="object-cover object-[0_2%]"
-              />
-            </CalendarCssArt>
-          </motion.div>
-
-          <motion.div
-            variants={itemVariants}
-            className="absolute z-10 left-[-25vw] sm:left-[-20vw] md:left-[-15vw] lg:left-[-10vw] top-[200px] sm:top-[180px] md:top-[300px]"
-          >
-            <CalendarCssArt>
-              <Image
-                src="/images/heroMobile2.png"
-                alt="eversports logo"
-                fill
-                className="object-cover object-[0_5%]"
-              />
-            </CalendarCssArt>
-          </motion.div>
+          <CalendarCssArt>
+            <Image
+              src="/images/heroMobile.png"
+              alt="eversports logo"
+              fill
+              className="object-cover object-[0_2%]"
+            />
+          </CalendarCssArt>
         </motion.div>
-      </div>
-    </section>
-  );
-};
+        <motion.div
+          variants={itemVariants}
+          className="absolute z-10 left-[-25vw] sm:left-[-20vw] md:left-[-15vw] lg:left-[-10vw] top-[200px] sm:top-[180px] md:top-[300px]"
+        >
+          <CalendarCssArt>
+            <Image
+              src="/images/heroMobile2.png"
+              alt="eversports logo"
+              fill
+              className="object-cover object-[0_5%]"
+            />
+          </CalendarCssArt>
+        </motion.div>
+      </motion.div>
 
 export default Hero;
